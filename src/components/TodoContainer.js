f25ec1a3218fbc6d018f899fb5f5db39102f97a6
AckonSamuel <ackons433@gmail.com>
2022-08-08 15:39:50 +0000

    refactor code to use prevState

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -25,14 +25,17 @@ class TodoContainer extends React.Component {
   };
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
+    this.setState(prevState => ({
+      todos: prevState.state.todos.map(todo => {
         if ( todo.id === id ) {
-          todo.completed = !todo.completed;
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
         }
         return todo;
       })
-    })
+    }))
   };
 
   render() {
