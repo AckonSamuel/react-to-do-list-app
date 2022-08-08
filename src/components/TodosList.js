c85c9c554c12206a1e2b26c85c7e8652b082069a
AckonSamuel <ackons433@gmail.com>
2022-08-08 13:46:36 +0000

    Update todolist component to render tasks

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,7 +3,13 @@ import React from "react"
 class TodosList extends  React.Component {
   render() {
     return (
-      <h1>hello</h1>
+     <ul>
+       {this.props.todos.map(todo => (
+         <li>{todo.title}</li>
+       ))}
+     </ul>
     )
   }
 }
+
+export default TodosList
