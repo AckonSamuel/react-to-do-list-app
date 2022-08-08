e0661217177fba197f7640125ca612b63d81f702
AckonSamuel <ackons433@gmail.com>
2022-08-08 14:46:44 +0000

    Make a controlled input

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from "react"
 class TodoItem extends React.Component {
   render() {
     return <li>
-      <input type="checkbox" /> {this.props.todo.title}
+      <input type="checkbox" 
+      checked={this.props.todo.completed}
+      onChange={() => console.log("clicked")} /> 
+       {this.props.todo.title}
       </li>
   }
 }
