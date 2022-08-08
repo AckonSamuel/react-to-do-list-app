da02719e5ff3d66a91e4203723d0629983da795c
AckonSamuel <ackons433@gmail.com>
2022-08-08 13:57:25 +0000

    Define todoitem component

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -5,7 +5,7 @@ class TodosList extends  React.Component {
     return (
      <ul>
        {this.props.todos.map(todo => (
-         <li key={todo.id}>{todo.title}</li>
+        <TodoItem key={todo.id} todo={todo} />
        ))}
      </ul>
     )
