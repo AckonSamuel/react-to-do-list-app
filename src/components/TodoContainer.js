c51b87d16e9c178e3a4687634aa3522954bdc2bf
AckonSamuel <ackons433@gmail.com>
2022-08-10 09:58:01 +0000

    Add class names

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -63,7 +63,8 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-     <div>
+     <div className="container">
+       <div className="inner">
        <Header />
        <InputTodo addTodoProps={this.addTodoItem} />
        <TodosList 
@@ -71,6 +72,7 @@ class TodoContainer extends React.Component {
        handleChangeProps={this.handleChange}
        deleteTodoProps={this.delTodo} 
        />
+     </div>
      </div>
     )
   }
