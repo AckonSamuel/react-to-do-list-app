2a99a7236d8200f0c862519784049ae6ed1595fc
AckonSamuel <ackons433@gmail.com>
2022-08-10 13:48:30 +0000

    Get from local storage and render

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -63,6 +63,15 @@ class TodoContainer extends React.Component {
     }
   }
 
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
   render() {
     return (
      <div className="container">
