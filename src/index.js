601c8bb63aaf8bb9eb125618d58e0519243f6597
AckonSamuel <ackons433@gmail.com>
2022-08-11 22:48:13 +0000

    Hot fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import TodoContainer from './functionBased/components/TodoContainer';
-import "./functionBased/App.css"
+import "./functionBased/components/App.css"
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
