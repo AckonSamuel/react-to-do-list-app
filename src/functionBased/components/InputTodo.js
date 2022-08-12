2eb3a347cc6b76ff0bb87ec0c7ca5649063e1b4c
AckonSamuel <ackons433@gmail.com>
2022-08-12 15:03:53 +0000

    Necessary update

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,82 +1,43 @@
 import React, { useState } from "react"
 
-// const InputTodo = () => {
-//   const [title, setTitle] = useState("")
-
-//   const onChange = e => {
-//     setTitle(e.target.value)
-//   }
-
-//   const handleSubmit = e => {
-//     e.preventDefault()
-//     if(title.trim()) {
-//       props.addTodoProps(title)
-//       setTitle("")
-//     } else {
-//       alert("Please write item")
-//     }
-
-//   }
-
-//   return (
-//     <form onSubmit={handleSubmit}
-//     className="form-container">
-//       <input
-//       type="text"
-//       className="input-text"
-//       placeholder="Add todo..."
-//       value={title}
-//       name="title"
-//       onChange={onChange}
-//       />
-//       <button className="input-submit">Submit</button>
-//       </form>
-//   )
-// }
-
 const InputTodo = props => {
-  const [ inputText, setInputText] = useState({
-    fName: "",
-    lastNmae: "",
+  const [inputText, setInputText] = useState({
+    title: "",
   })
 
   const onChange = e => {
-    setInputText({
-      ...inputText,
-      [e.target.name]: e.target.value,
-    })
+   setInputText({
+     ...inputText,
+     [e.target.name]: e.target.value,
+   })
   }
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log("submitted")
+    if(inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
+
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}
-      className="form-container">
-        <input
-        type="text"
-        className="input-text"
-        placeholder="Add first name"
-        name="fName"
-        onChange={onChange}
-        />
-        <input
-        type="text"
-        className="input-text"
-        placeholder="Add last name"
-        value={inputText.lastName}
-        name="lastName"
-        onChange={onChange}
-        />
-
-        <button className="input-submit">Submit</button>
-        </form>
-        <h2>{inputText.fName}</h2>
-        <h2>{inputText.lastName}</h2>
-    </>
+    <form onSubmit={handleSubmit}
+    className="form-container">
+      <input
+      type="text"
+      className="input-text"
+      placeholder="Add todo..."
+      value={inputText.title}
+      name="title"
+      onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+      </form>
   )
 }
 
