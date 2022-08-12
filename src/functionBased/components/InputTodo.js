a8000cee1be0bed68e1ad0ee859159bc6ef33dfa
AckonSamuel <ackons433@gmail.com>
2022-08-12 14:27:53 +0000

    Update InputTodo component

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,8 +1,37 @@
 import React, { useState } from "react"
 
 const InputTodo = () => {
-  console.log(useState("hello"))
-  return <div></div>
+  const [title, setTitle] = useState("")
+
+  const onChange = e => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(title.trim()) {
+      props.addTodoProps(title)
+      setTitle("")
+    } else {
+      alert("Please write item")
+    }
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}
+    className="form-container">
+      <input
+      type="text"
+      className="input-text"
+      placeholder="Add todo..."
+      value={title}
+      name="title"
+      onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+      </form>
+  )
 }
 
 export default InputTodo
