ac1f4e08d927fa6cf3b6834cd67a516d25a4c61d
AckonSamuel <ackons433@gmail.com>
2022-08-12 15:28:42 +0000

    1 file modified

--- src/classBased/components/TodoContainer.js
+++ src/classBased/components/TodoContainer.js
@@ -4,14 +4,14 @@ import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 
-class TodoContainer extends React.Component {
+const TodoContainer = () => {
 
-  state = {
-    todos : [],
-  };
+  const [ task, updateTask ] = useState({
+    todos: [],
+  })
 
-  handleChange = (id) => {
-    this.setState(prevState => ({
+  const handleChange = (id) => {
+    updateTask(prevState => ({
       todos: prevState.todos.map(todo => {
         if ( todo.id === id ) {
           return {
@@ -24,71 +24,21 @@ class TodoContainer extends React.Component {
     }))
   };
 
-  delTodo = id => {
-    this.setState({
+  const delTodo = id => {
+    updateTask({
       todos: [
-        ...this.state.todos.filter(todo => {
+        ...task.todos.filter(todo => {
           return todo.id !== id;
         })
       ]
     });
   };
 
-  addTodoItem = title => {
+  const addTodoItem = title => {
     const newTodo = {
       id: uuidv4(),
       title: title,
       completed: false
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
-  };
-
-  setUpdate = (updatedTitle, id) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        todo.id == id ?
-        todo.title = updatedTitle:
-        todo.title = todo.title
-        return todo
-      }),
-    })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.todos !== this.state.todo) {
-      const temp = JSON.stringify(this.state.todos)
-      localStorage.setItem("todos", temp)
     }
-  }
-
-  componentDidMount() {
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
-    if (loadedTodos) {
-      this.setState({
-        todos: loadedTodos
-      })
-    }
-  }
-
-  
-  render() {
-    return (
-     <div className="container">
-       <div className="inner">
-       <Header />
-       <InputTodo addTodoProps={this.addTodoItem} />
-       <TodosList 
-       todos = {this.state.todos} 
-       handleChangeProps={this.handleChange}
-       deleteTodoProps={this.delTodo} 
-       setUpdate={this.setUpdate}
-       />
-     </div>
-     </div>
-    )
   }
 }
-export default TodoContainer
